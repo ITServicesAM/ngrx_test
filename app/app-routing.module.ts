e2612bf3b06665c0ce6dd7cca59f15e9dbea2b71
Armin Maaßen <armin.maassen1989@gmail.com>
2018-03-12 21:03:48 +0100

    no message

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { FirebaseFilteringModule } from './firebase-filtering/firebase-filtering.module';
 
 const routes: Routes = [
-    { path: "", redirectTo: "/home", pathMatch: "full" },
-    { path: "home", loadChildren: "./home/home.module#HomeModule" }
+    { path: "", redirectTo: "/fb-filtering", pathMatch: "full" },
+    { path: "home", loadChildren: "./home/home.module#HomeModule" },
+    { path: "fb-filtering", loadChildren: "./firebase-filtering/firebase-filtering.module#FirebaseFilteringModule" }
 ];
 
 @NgModule({
--- app/firebase-filtering/firebase-filtering/firebase-filtering.component.ts
+++ app/firebase-filtering/firebase-filtering/firebase-filtering.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { IAppState } from '../../reducers';
+import { Store } from '@ngrx/store';
+import * as fromFbFiltering from '../store/firebase-filtering.reducer';
+import * as fbFilteringActions from '../store/firebase-filtering.actions';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-firebase-filtering',
-  templateUrl: './firebase-filtering.component.html',
-  styleUrls: ['./firebase-filtering.component.css']
+    moduleId: module.id,
+    selector: 'app-firebase-filtering',
+    templateUrl: './firebase-filtering.component.html',
+    styleUrls: ['./firebase-filtering.component.css']
 })
 export class FirebaseFilteringComponent implements OnInit {
 
-  constructor() { }
+    constructor(private store: Store<IAppState>) {
+    }
 
-  ngOnInit() { }
-
+    ngOnInit() {
+        this.store.select(fromFbFiltering.getAllHomes)
+            .subscribe(val => console.log(`FbFiltering: ${JSON.stringify(val)}`));
+        this.store.dispatch(new fbFilteringActions.Query('homes'));
+    }
 }
